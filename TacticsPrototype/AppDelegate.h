04d95044924537555675a4bb32c41dad0ed4c190
Chris Meill <cmeill@Chriss-MacBook-Pro.local>
2015-06-22 10:28:21 -0700

    Initial Commit

---
+++ TacticsPrototype/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TacticsPrototype
+//
+//  Created by Chris Meill on 6/22/15.
+//  Copyright (c) 2015 Asgardian Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TacticsPrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TacticsPrototype
+//
+//  Created by Chris Meill on 6/22/15.
+//  Copyright (c) 2015 Asgardian Games. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
